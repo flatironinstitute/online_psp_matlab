e15d5937bc9266f5f8f1f054ff3de27438a09f69
Andrea Giovannnucci <andrea.giovannucci@gmail.com>
2016-01-28 16:11:29 -0500

    new graphicval result representation

--- plot_results.m
+++ plot_results.m
@@ -1,23 +1,56 @@
 %% display results simulation
 files=dir('*.mat')
-close all
+figure
 hold all
 legends={};
-cm=colormap(hot(20))
+cm=colormap(hot(25))
+d_s=[];
+q_s=[];
+err_s=[];
+errhalf_s=[];
 for f=1:numel(files)
    disp(files(f).name)
    load(files(f).name,'d','q','errors')
    legends{f}=files(f).name;
    plot(median(errors,2),'d','Linewidth',2,'Color',cm(f,:))
-   legend(legends{:})   
+   err_s=[ err_s errors(end,:)'];
+   d_s=[d_s d];
+   q_s=[q_s q];
+   legend(legends{:},'Interpreter', 'none')   
 %    input('')
+%    cla
 end
 legend(legends, 'Interpreter', 'none')
 xlabel('Samples')
 ylabel('Eigenspace Estimation Error pca vs batch')
 %%
 files=dir('*.mat')
-close all
+figure
+hold all
+legends={};
+cm=colormap('lines')
+% cm=colormap(hot(20))
+colq=[];
+for f=1:numel(files)
+   disp(files(f).name)
+   load(files(f).name,'d','q','errors')
+   legends{f}=files(f).name;
+   legends{f}=['input dim=' num2str(d)];
+   colq(f)=errorbar(q+normrnd(0,q/50,size(q)),nanmedian(errors(end,:)),quantile(errors(end,:),.25),quantile(errors(end,:),.75),'ko','MarkerFaceColor',cm(log2(d),:),'MarkerSize',10) ;
+   set(gca,'yscale','log')
+   set(gca,'xscale','log')
+%    legend(legends{:})   
+% input('')
+end
+[vals,idx]=unique(d_s)
+legend(colq(idx),legends{idx}, 'Interpreter', 'none')
+xlabel(['output dim = [' num2str(unique(q_s)) ']'], 'Interpreter', 'none')
+ylabel('Projection Error')
+saveas(gcf,'ProjErrors.jpg')
+saveas(gcf,'ProjErrors.fig')
+%%
+files=dir('*.mat')
+figure
 hold all
 legends={};
 cm=colormap('lines')
@@ -27,21 +60,18 @@ for f=1:numel(files)
    disp(files(f).name)
    load(files(f).name,'d','q','times_')
    legends{f}=files(f).name;
-   legends{f}=num2str(d);
-   colq(f)=errorbar(q+normrnd(0,.02,size(q)),nanmedian(times_(:))*1000,iqr(times_(:)*1000),'ko','MarkerFaceColor',cm(log2(d),:)) ;
+   legends{f}=['input dim=' num2str(d)];
+   colq(f)=errorbar(q+normrnd(0,q/50,size(q)),nanmedian(times_(:)*1000),quantile(times_(:)*1000,.25),quantile(times_(:)*1000,.75),'ko','MarkerFaceColor',cm(log2(d),:),'MarkerSize',10) ;
    set(gca,'yscale','log')
    set(gca,'xscale','log')
 %    legend(legends{:})   
-%    input('')
+% input('')
 end
-legend(colq,legends{:})
-xlabel('q')
+[vals,idx]=unique(d_s)
+legend(colq(idx),legends{idx}, 'Interpreter', 'none')
+xlabel(['output dim = [' num2str(unique(q_s)) ']'], 'Interpreter', 'none')
 ylabel('Time per iteration (ms)')
-
-%%
-ax=gca;
-ax.XTick=[1:numel(legends)];
-ax.XTickLabel = legends;
-ax.XTickLabelRotation = 45;
+saveas(gcf,'TimeIter.jpg')
+saveas(gcf,'TimeIter.fig')
 
 
