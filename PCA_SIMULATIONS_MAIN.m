d9c725eab1c2bddfa56af2dc2d2fc0fe258b85ce
Andrea Giovannnucci <andrea.giovannucci@gmail.com>
2016-01-28 16:12:29 -0500

    updated simulation file and created new one for GAP simulations

--- PCA_SIMULATIONS_MAIN.m
+++ PCA_SIMULATIONS_MAIN.m
@@ -1,16 +1,11 @@
 clear all
 profile off
 profile on
-niter=100; % was 40
+niter=40; % was 40
 n0 = 0; % number of sample paths used for initialization
 q_each=[2 4 16 64 256 512];
-num_samples_each=1024%8192;
+num_samples_each=8192;
 d_each=[4 16 64 256 1024 4096];
-if n0==50
-    learning_rates=[10,1,.1]/5; 
-else 
-    learning_rates=[10,1,.1];
-end
 
 nstep_skip_EIGV_errors=50;
 close all
@@ -36,7 +31,7 @@ errors_paper=[.011 .007 .007];
 errors_paper_half=[0.02 .015 .015];
 times_paper=[.17  .11  .17 ];
 [allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper);
-close all
+    close all
 %%
 options=struct();
 test_method='IPCA';
@@ -48,25 +43,25 @@ times_paper=[.17  .11  .17 ];
 [allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper);
 close all
 %% !!!! LEARNING RATE MANAULLY SET IN THE FILE!!!
-options=struct();
-test_method='SGA'; 
-options.method=test_method;
-options.do_sort=1;
-errors_paper=[ .025 .014 .016];
-errors_paper_half=[0.031 .02 .021];
-
-times_paper=[0.09 .16 .12];
-[allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper,learning_rates);
-close all
-%% !!!! LEARNING RATE MANAULLY SET IN THE FILE
-
-options=struct();
-test_method='GHA'; 
-options.method=test_method;
-options.do_sort=1;
-errors_paper=[ .024 .014 .016];
-errors_paper_half=[0.03 .02 .023 ];
-
-times_paper=[0.06 .08 .09 ];
-[allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper,learning_rates);
-close all
+% options=struct();
+% test_method='SGA'; 
+% options.method=test_method;
+% options.do_sort=1;
+% errors_paper=[ .025 .014 .016];
+% errors_paper_half=[0.031 .02 .021];
+% 
+% times_paper=[0.09 .16 .12];
+% [allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper);
+% close all
+% %% !!!! LEARNING RATE MANAULLY SET IN THE FILE
+% 
+% options=struct();
+% test_method='GHA'; 
+% options.method=test_method;
+% options.do_sort=1;
+% errors_paper=[ .024 .014 .016];
+% errors_paper_half=[0.03 .02 .023 ];
+% 
+% times_paper=[0.06 .08 .09 ];
+% [allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper,learning_rates);
+% close all
---
+++ PCA_SIMULATIONS_MAIN_GAP.m
@@ -0,0 +1,81 @@
+clear all
+profile off
+profile on
+niter=20; % was 40
+n0 = 0; % number of sample paths used for initialization
+q_each=16;
+num_samples_each=1024;
+d_each=[256];
+
+nstep_skip_EIGV_errors=50;
+close all
+%%
+% options_generator=struct;
+% options_generator.method='brownian_motion';
+%%
+options_generator=struct;
+options_generator.method='spiked_covariance';
+options_generator.rho=0.1;
+options_generator.gap=.5;
+options_generator.slope=.05;
+%%
+options_generator=struct;
+options_generator.method='spiked_covariance_normalized';
+% options_generator.rho=0.1;
+options_generator.lambda_q=.95;
+%%
+for rho=[.3]
+    disp(gap)
+    options_generator.rho=rho;
+    options=struct();
+    test_method='H_AH_NN_PCA';
+    options.tol=1e-5;
+    % options.update_method='mat_mult';
+    % options.mat_iter=10;
+    options.update_method='ls';
+    errors_paper=[.011 .007 .007];
+    errors_paper_half=[0.02 .015 .015];
+    times_paper=[.17  .11  .17 ];
+    [allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper);
+    hold all
+end
+%%
+for gap=[0.1 10]
+    disp(gap)
+    options_generator.gap=gap;
+options=struct();
+test_method='IPCA';
+options.tol=1e-7;
+errors_paper=[.011 .007 .007];
+errors_paper_half=[0.02 .015 .015];
+
+times_paper=[.17  .11  .17 ];
+[allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper);
+end
+%% !!!! LEARNING RATE MANAULLY SET IN THE FILE!!!
+for gap=[0.1 10]
+    disp(gap)
+    options_generator.gap=gap;
+    
+    options=struct();
+    test_method='SGA';
+    options.method=test_method;
+    options.do_sort=1;
+    errors_paper=[ .025 .014 .016];
+    errors_paper_half=[0.031 .02 .021];
+    
+    times_paper=[0.09 .16 .12];
+    [allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper);
+end
+%% !!!! LEARNING RATE MANAULLY SET IN THE FILE
+
+options=struct();
+test_method='GHA'; 
+options.method=test_method;
+options.do_sort=1;
+errors_paper=[ .024 .014 .016];
+errors_paper_half=[0.03 .02 .023 ];
+
+times_paper=[0.06 .08 .09 ];
+[allerrors,alltimes,legends]=Online_PCA_simulations(q_each,num_samples_each,d_each,n0,niter,nstep_skip_EIGV_errors,test_method,options,options_generator,errors_paper,errors_paper_half,times_paper,learning_rates);
+close all
