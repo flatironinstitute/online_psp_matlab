eadec162981f35a7bc99aad2b2bb7c4f0975ba38
Andrea Giovannucci <agiovann@AndreaGiovannucci2015RetinaMacbookPro.local>
2016-03-15 12:06:12 -0400

    work in progress nonnegative online

--- MNIST/H_AH_MNIST.m
+++ MNIST/H_AH_MNIST.m
@@ -1,9 +1,11 @@
 clear all
 x1 = loadMNISTImages('train-images-idx3-ubyte');
 x=bsxfun(@minus,x1,mean(x1,2));
+% eps_std=.1;
+% x=bsxfun(@times,x,1./(eps_std+std(x,[],2)));
 if 0
     sigma = x * x' / size(x, 2);
-    k=50;
+    k=128;
     [U,S,V] = svds(sigma,k);
     epsilon=0;
     x = U * diag(1./sqrt(diag(S) + epsilon)) * U' * x;
@@ -13,7 +15,7 @@ end
 %%
 tic
 [d,T]=size(x);
-q=50;
+q=64;
 W=rand(q,d)*.01;
 M=rand(q,q)*.01;
 M=M-diag(diag(M));
@@ -41,6 +43,8 @@ toc
 %%
 tic
 [W1,Y1_tot]=pca(x','NumComponents',q);
+% [Y1_tot,W1]=nnmf(x',q);
+
 W1=W1';
 toc
 %%
@@ -48,21 +52,22 @@ norm(x-W1'*W1*x)/norm(x)
 %%
 norm(x-orth(W')*orth(W')'*x)/norm(x)
 %%
-T1=kmeans(Y1_tot,10,'Replicates',20);
+T1=kmeans(Y1_tot,10,'Replicates',10);
 %%
 Y_tot=(eye(q)+M)\W*x;
-T=kmeans(Y_tot',10);
+T=kmeans(Y_tot',10,'Replicates',10);
 %%
 d1=sqrt(d);
 Y_tot=(eye(q)+M)\W*x;
 [val,idx]=max(Y_tot,[],1);
 for jj=1:10
     subplot(4,3,jj)
-    imagesc(reshape(mean(x1(:,T1==jj),2),[d1,d1]))
+    imagesc(reshape(mean(x1(:,T==jj),2),[d1,d1]))
 end
 %%
 lab=loadMNISTLabels('train-labels-idx1-ubyte');
 MIhat = MutualInfo(T1-1,lab)
+MIhat = MutualInfo(T-1,lab)
 %%
 
 for jj=0:9
