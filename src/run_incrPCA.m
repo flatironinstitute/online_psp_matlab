82b1972ef0f57bd776597b68d37b3cf0da8097e1
Andrea Giovannucci <agiovann@AndreaGiovannucci2015RetinaMacbookPro.local>
2016-04-08 10:40:20 -0400

    working on the performance of ORL dataset

---
+++ src/run_incrPCA.m
@@ -0,0 +1,54 @@
+function [values,vectors,iter_final]=run_incrPCA(x,q,n_init_PCA,values,vectors,iter_so_far,options_algorithm)
+
+if ~exist('options_algorithm')
+    options_algorithm=struct();
+    options_algorithm.pca_algorithm='IPCA';    
+    options_algorithm.tol=1e-7;
+end
+
+options_algorithm.q=q;
+[d,T]=size(x);
+scramble=randperm(T);
+x=x(:,scramble);
+
+    
+if n_init_PCA>0
+    if n_init_PCA<q
+        error('n_init_PCA cannot be smaller than q')
+    end
+    if ~isempty(vectors) ||  ~isempty(values) ||  ~isempty(iter_so_far)
+        error('you cannot have n_init_PCA >0 and pass inputs vectors, values , iter_so_far')
+    end
+
+    n0=n_init_PCA;
+    [eigvect_init,~,eigval_init]=pca(x(:,1:(n0+1))','NumComponents',q);     
+    vectors=eigvect_init; 
+    values=eigval_init(1:q); 
+else
+    if isempty(values) || isempty(vectors) 
+        n0=1;
+        values=normrnd(0,.1,q,1);
+        vectors=normrnd(0,.1,d,q);
+        iter_so_far=0;
+        disp('Initializing eigenvalues and eigenvectors randomly')    
+    end
+end
+
+
+if isempty(iter_so_far)
+   iter_so_far=0; 
+end
+
+
+for iter=(n_init_PCA+1):(T)    
+    if mod(iter,100)==0
+        disp(iter_so_far+iter-1)
+    end
+    options_algorithm.n=max(1,iter_so_far+iter-1);
+    
+    [values, vectors] = incrPCA_fast(values, vectors, x(:,iter)', options_algorithm);        
+end
+
+iter_final=iter;
+
+
